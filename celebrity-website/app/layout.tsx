06fa3caf73406fafa40195bf9cd39fd643853668
youngboypimpin <rjtekdom@gmail.com>
2024-05-12 14:04:38 +0100

    added boxicons folder to the project folder

--- celebrity-website/app/layout.tsx
+++ celebrity-website/app/layout.tsx
@@ -1,5 +1,5 @@
 import type { Metadata } from "next";
-import "./globals.css";
+import "./style.css";
 
 
 export const metadata: Metadata = {
@@ -14,7 +14,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body >{children}</body>
+      <head>
+      <meta charSet="UTF-8"></meta>
+      <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      <title>Animated Portfolio Website HTML CSS JS</title>
+      <link href='boxicons.min.css' rel='stylesheet'></link>
+      </head>
+      <body  className="body">{children}</body>
     </html>
   );
 }
--- celebrity-website/app/page.tsx
+++ celebrity-website/app/page.tsx
@@ -1,19 +1,9 @@
 import Image from "next/image";
-import '@/app/page.module.css';
-
 
 export default function Landing() {
   return (
-    <><head>
-      <meta charSet="UTF-8"></meta>
-      <meta http-equiv="X-UA-Compatible" content="IE=edge"></meta>
-      <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
-      <title>Animated Portfolio Website HTML CSS JS</title>
-      <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'></link>
-      <link rel="stylesheet" href="style.css"></link>
-      </head><body>
-
-        <header className="header">
+    <>
+      <header className="header">
           <a href="#" className="logo">Patrick</a>
           <nav className="navbar">
             <a href="#" className="active">Home</a>
@@ -47,29 +37,9 @@ export default function Landing() {
           <span className="home-imgHover"></span>
         </section>
 
-        <section className="home">
-          <div className="home-content">
-            <h3>Services</h3>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nam facilis ea
-              deserunt sed dignissimos temporibus enim quos impedit perferendis iste accusamus fuga,
-              ratione quisquam aliquam incidunt quaerat, sit dolore asperiores.
-            </p>
-            <div className="btn-box">
-              <a href="">Hire Me</a>
-              <a href="">Let's Talk</a>
-            </div>
-          </div>
-          <div className="home-sci">
-            <a href=""><i className='bx bxl-linkedin'></i></a>
-            <a href=""><i className='bx bxl-twitter'></i></a>
-            <a href=""><i className='bx bxl-github'></i></a>
-          </div>
-
-        </section>
 
         <script src="https://cdn.jsdelivr.net/npm/typed.js@2.0.12"></script>
         <script src="script.js"></script>
-      </body></>
+      </>
   );
 }
